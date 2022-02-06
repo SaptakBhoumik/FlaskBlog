e7fe453fcfa62173e4c565bebe0e287839f4edf2
Saptak Bhoumik <saptakbhoumik@gmail.com>
2022-02-06 21:04:46 +0530

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -25,11 +25,12 @@ class User(db.Model):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __init__(self):
+        self.id = db.Column(db.Integer, primary_key=True)
+        self.title = db.Column(db.String(100), nullable=False)
+        self.date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+        self.content = db.Column(db.Text, nullable=False)
+        self.user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
